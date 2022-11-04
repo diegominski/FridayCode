f590b07fd4ec13a27ca2c62db50eea6d0d0fe707
diegominski <70980010+diegominski@users.noreply.github.com>
2022-11-04 11:08:10 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+let arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let speeds = [1, 1.2, 1.4, 1.6, 1.8, 2, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.6, 3.8, 4.0, 4.2, 4.4, 4.6, 4.8, 5.0, 5.2];
+
+function setup() {
+    let canvas = createCanvas(windowWidth, windowHeight)
+    canvas.parent('myContainer');
+}
+
+function draw(){
+    background(0, 0, 0, 1);
+  
+      }
